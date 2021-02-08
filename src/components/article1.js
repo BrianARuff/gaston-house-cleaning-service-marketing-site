00c7fdf71ee9451b16dc6de88ea958d766cfa43c
Brian Ruff <briananthonyruff@gmail.com>
2021-02-07 23:01:01 -0500

    fixed spacing error with location indiactor

--- src/components/article1.js
+++ src/components/article1.js
@@ -4,13 +4,11 @@ const article1 = props => {
   return (
     <article className="left">
       <h2 className="left-header">We do the Heavy Lifting for You</h2>
-      <p className="left-description">
+      <div className="left-description">
         Gaston Home Cleaning Services will do the heavy lifting for you. You
         deserve a clean home, let us work hard for you.{" "}
-
-         We offer in home cleaning services in the{" "}</p>
-        <div className={"tool-tip"}>
-          <strong>Charlotte Metropolitan Area</strong>
+         We offer in home cleaning services in the{" "} <div style={{display: `inline`}} className={"tool-tip"}>
+        <strong>Charlotte Metropolitan Area</strong>
           <div className={"tool-tip-text"}>
             <ul>
               <li>Concord</li>
@@ -20,7 +18,8 @@ const article1 = props => {
               <li>Mecklenburg County</li>
             </ul>
           </div>
-        </div>
+        </div></div>
+        <br></br>
       <p>
         <strong>
           <a href="tel:980-240-6927">Schedule Appointment</a>
